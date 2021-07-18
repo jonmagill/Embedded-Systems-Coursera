69459e042ceff205c134d6fbafec35dc48effe80
Jonathan Magill <jonmagill@gmail.com>
2021-07-18 14:41:30 -0600

    Creating Initial Coursera Repository

---
+++ README.md
@@ -0,0 +1,17 @@
+*******************************************************************************
+# Copyright (C) 2021 - Jonathan Magill
+
+# Redistribution, modification, or use of these files are not permitted (and are
+# probably of little value) other than for the purposes of assessment of the 
+# work contained within them.
+
+# Jonathan Magill is not liable for any misuse of this material.
+
+********************************************************************************
+
+### This is a repository to contain work committed as part of the Introduction
+### to Embedded Systems Software and Development Environments course on 
+### Coursera.
+
+
+
---
+++ stats.c
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.c
+ * @brief Carries out a set of statistical operations on a set of input data
+ * and prints the results
+ *
+ * Takes an array of unsigned char data items and calculates:
+ *   - maximum
+ *   - minimum
+ *   - mean
+ *   - median
+ * (rounded down to the nearest int)
+ *
+ * The data is sorted from largest to smallest and all the data is printed to
+ * the screen.
+ *
+ * @author Jonathan Magill
+ * @date 18/07/21
+ *
+ */
+
+
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+
+}
+
+/* Add other Implementation File Code Here */
